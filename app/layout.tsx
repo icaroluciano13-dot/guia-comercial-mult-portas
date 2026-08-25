117ecdc35796b57f5275275c6350569e062f49e4
icaroluciano13-dot <icaroluciano13@icloud.com>
2026-08-25 10:52:16 -0300

    Aplica nova terminologia de prestadores no site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { TerminologyNormalizer } from "./terminology-normalizer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <TerminologyNormalizer />
         {children}
       </body>
     </html>
